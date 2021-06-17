9eb0c71ab52e73856243499c2e1cbe542df7de3d
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2021-06-17 21:00:39 +0700

    Add router

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 const express = require('express')
 
 const app = express()
+const router = express.Router()
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: false}))
 
 const data = []
 
-app.get('/users', (req, res, next) => {
+router.get('/users', (req, res, next) => {
     res.render('users', {pageTitle: 'Users', path: '/users', data: data})
 })
 
-app.get('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
     res.render('add-user' , {pageTitle: 'Add User', path: '/'})
 })
 
-app.post('/add-user', (req, res, next) => {
+router.post('/add-user', (req, res, next) => {
     data.push({name: req.body.name})
     res.redirect('/users')
 })
+app.use(router)
 
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: 'Not Found'})
+})
 app.listen(3000)
 
